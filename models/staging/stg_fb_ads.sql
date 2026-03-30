78008379e50cfca57512f7b6621cd906a44744eb
Esther-joy <104839668+EstherJoyMackay@users.noreply.github.com>
2026-03-30 12:43:30 +0100

    Update stg_fb_ads.sql

--- models/staging/stg_fb_ads.sql
+++ models/staging/stg_fb_ads.sql
@@ -1,9 +1,9 @@
 select
     day as ad_date,
-    "campaign_name",
-    "costs",
-    "impressions",
+    campaign_name,
+    costs,
+    impressions,
     "clicks",
-    "device_type" as device,
+    device_type as device,
     'Facebook' as platform
 from {{ source('ad_data', 'ads_insights_1166') }}
