debad9cc2e45726cc37114460dcccc9133d97958
Esther-joy <104839668+EstherJoyMackay@users.noreply.github.com>
2026-03-30 12:11:21 +0100

    Update fct_marketing_performance.sql

--- models/marts/fct_marketing_performance.sql
+++ models/marts/fct_marketing_performance.sql
@@ -1,1 +1,18 @@
+with combined as (
+    select * from {{ ref('stg_fb_ads') }}
+    union all
+    select * from {{ ref('stg_google_ads') }}
+)
 
+select
+    ad_date,
+    platform,
+    device,
+    sum(costs) as total_costs,
+    sum(clicks) as total_clicks,
+    -- KPI: Cost Per Click
+    sum(costs) / nullif(sum(clicks), 0) as cpc,
+    -- KPI: Click-Through Rate
+    (sum(clicks) * 1.0) / nullif(sum(impressions), 0) as ctr
+from combined
+group by 1, 2, 3
