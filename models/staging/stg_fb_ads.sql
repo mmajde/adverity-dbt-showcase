173b207ca88341426a720838403285da29f925b7
Esther-joy <104839668+EstherJoyMackay@users.noreply.github.com>
2026-03-30 12:56:48 +0100

    Update stg_fb_ads.sql

--- models/staging/stg_fb_ads.sql
+++ models/staging/stg_fb_ads.sql
@@ -3,7 +3,6 @@ select
     campaign_name,
     costs,
     impressions,
-    CLICKS as clicks,
     device_type as device,
     'Facebook' as platform
 from {{ source('ad_data', 'ads_insights_1166') }}
