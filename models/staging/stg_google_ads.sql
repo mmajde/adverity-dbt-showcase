cfb62ad33952396258515240208b0922ef5dc789
Esther-joy <104839668+EstherJoyMackay@users.noreply.github.com>
2026-03-30 12:09:33 +0100

    Update stg_google_ads.sql

--- models/staging/stg_google_ads.sql
+++ models/staging/stg_google_ads.sql
@@ -1,1 +1,9 @@
-
+select
+    Day as ad_date,
+    campaign_name as campaign_name,
+    costs as spend,
+    impressions as impressions,
+    Clicks as clicks,
+    device_type as device,
+    'Google' as platform
+from {{ source('ad_data', 'google_ads') }}
